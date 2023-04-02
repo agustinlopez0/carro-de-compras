cea81857e5b1f765f09388ebce49f69d7199a477
agustinlopez0 <70503126+agustinlopez0@users.noreply.github.com>
2023-04-01 21:11:12 -0300

    cap 200 completo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Productos from './components/Productos'
 import Layout from './components/Layout'
 import Title from './components/Title'
+import Navbar from './components/Navbar'
 
 class App extends Component {
   state = {
@@ -9,15 +10,38 @@ class App extends Component {
       { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
       { name: 'Arvejas', price: 2500, img: '/productos/arvejas.jpg' },
       { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
-    ]
+    ],
+    carro: [],
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find( x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1 
+        })
+        : x )
+        return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.carro)
     return (
       <div>
+        <Navbar carro={this.state.carro}/>
         <Layout>
           <Title />
           <Productos 
-            agregarAlCarro={() => console.log('No hace nada')}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
